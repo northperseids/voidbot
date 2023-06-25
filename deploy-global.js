7a32d0852b429bc2614649925a26dc05bcff65ba
E. North <109831425+northperseids@users.noreply.github.com>
2023-06-25 12:49:52 -0400

    Update deploy-global.js
    Adds commands and restructures the deploy script for Void.

--- deploy-global.js
+++ deploy-global.js
@@ -1,43 +1,62 @@
 require('dotenv').config();
-const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+const { REST, Routes, ApplicationCommandOptionType, SlashCommandBuilder } = require('discord.js');
 
-const commands = [
-    {
-        name: 'information',
-        description: 'Gives info about the bot!'
-    },
-    {
-        name: 'yelling',
-        description: 'Yell into the void.',
-        options: [{
-            name: 'yell',
-            description: 'Yell something, okay?',
-            type: ApplicationCommandOptionType.String,
-            required: true,
-        }
-    ]
-    },
-    {
-        name: 'say-something',
-        description: 'Say something into the void.',
-        options: [{
-            name: 'say',
-            description: 'What would you like to say?',
-            type: ApplicationCommandOptionType.String,
-            required: true,
-        }
-    ]
-    },
-    {
-        name: 'pop',
-        description: 'Need some bubble wrap?',
-    },
-    {
-        name: 'self-care',
-        description: 'Gives a random self-care task!',
-    }
-];
+const commands = [];
 
+const info = new SlashCommandBuilder()
+    .setName('information')
+    .setDescription('Gives info about the bot!')
+
+const help = new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Gives a list of all available commands!')
+
+const pop = new SlashCommandBuilder()
+    .setName('pop')
+    .setDescription('Need some bubble wrap?')
+
+const selfcare = new SlashCommandBuilder()
+    .setName('self-care')
+    .setDescription('Gives a random self-care task!')
+
+const yelling = new SlashCommandBuilder()
+    .setName('yelling')
+    .setDescription('Yell into the void.')
+    .addStringOption(
+        option =>
+            option.setName('yell')
+                .setDescription('Yell something, okay?')
+                .setRequired(true)
+    )
+
+const say = new SlashCommandBuilder()
+    .setName('say-something')
+    .setDescription('Say something into the void.')
+    .addStringOption(
+        option =>
+            option.setName('say')
+                .setDescription('What would you like to say?')
+                .setRequired(true)
+    )
+
+const breathing = new SlashCommandBuilder()
+    .setName('breathing')
+    .setDescription(`Let's do a breathing exercise!`)
+
+const grounding = new SlashCommandBuilder()
+    .setName('grounding')
+    .setDescription(`Let's do some grounding!`)
+
+commands.push(
+    info.toJSON(),
+    help.toJSON(),
+    pop.toJSON(),
+    selfcare.toJSON(),
+    yelling.toJSON(),
+    say.toJSON(),
+    breathing.toJSON(),
+    grounding.toJSON()
+);
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
